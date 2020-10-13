5dcd4b4a6666b709f215c7c4ede6870cf9e5d165
Seido Kaito <38904618+S-Kaito@users.noreply.github.com>
2020-10-13 14:22:09 +0900

    Add parent component

--- sketch.js
+++ sketch.js
@@ -2,41 +2,77 @@ function easeInOutQuad(x) {
 	return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
 }
 
-console.log(easeInOutQuad(0), easeInOutQuad(1))
+class Component{
 
-class Rectangle{
+	onClick = null;
+
+	_prevMouseIsPressed = false;
+
+	_shouldEventExecute(){
+		if(!this._prevMouseIsPressed && mouseIsPressed && this.isClicked(mouseX, mouseY))
+			this.onClick && this.onClick();
+		this._prevMouseIsPressed = mouseIsPressed;
+	}
+
+	isClicked(x, y){
+		return false;
+	};
+
+	draw(){
+		this._shouldEventExecute();
+	}
+}
+
+class Rectangle extends Component{
+	mx = 100;
+	my = 100;
+
 	x = 100;
 	y = 100;
+	width = 300;
+	height = 300;
 
-	f = 0;
 	isOpen = false;
-	constructor(x, y){
+	frameRate = 50;
+	f = 50;
+	constructor(x, y, onClick){
+		super();
 		this.x = x;
 		this.y = y;
+		this.mx = x;
+		this.my = y;
+		this.onClick = onClick;
 	}
 
 	open(){
 		if(this.isOpen) return;
 		this.isOpen = true;
-		this.f = 100;
+		this.f = 0;
 	}
 
 	close(){
 		if(!this.isOpen) return;
 		this.isOpen = false;
-		this.f = 100;
+		this.f = 0;
+	}
+
+	isClicked(mouseX, mouseY){
+		const fragX = this.x < mouseX && mouseX < this.x + this.width;
+		const fragY = this.y < mouseY && mouseY < this.y + this.height;
+		return fragX && fragY;
 	}
 
 	draw(){
-		if(this.isOpen && this.f != 0){
-			this.x += easeInOutQuad(this.f / 100) * 5;
-			this.f -= 1;
+		super.draw();
+		if(this.isOpen && this.f != this.frameRate){
+			this.mx = this.x + this.width * easeInOutQuad(this.f / this.frameRate);
+			this.f += 1;
 		}
-		if(!this.isOpen && this.f != 0){
-			this.x -= easeInOutQuad(this.f / 100) * 5;
-			this.f -= 1;
+		if(!this.isOpen && this.f != this.frameRate){
+			this.mx = (this.x + this.width) - this.width * easeInOutQuad(this.f / this.frameRate);
+			this.f += 1;
 		}
-		rect(this.x, this.y, 300, 100);
+		rect(this.mx, this.my, this.width, this.height);
 	}
 }
 
@@ -44,7 +80,7 @@ var rec;
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
-	rec = new Rectangle(windowWidth - 300, 100);
+	rec = new Rectangle(windowWidth - 300, 100, () => {console.log("Hello, world!!")});
 }
 
 function draw(){
@@ -53,9 +89,9 @@ function draw(){
 }
 
 function keyPressed() {
-	if (keyCode  === RIGHT_ARROW && rec.f === 0) {
+	if (keyCode  === RIGHT_ARROW && rec.f === rec.frameRate) {
 		rec.open();
-	} else if (keyCode  === LEFT_ARROW && rec.f === 0) {
+	} else if (keyCode  === LEFT_ARROW && rec.f === rec.frameRate) {
 		rec.close();
 	}
 }
